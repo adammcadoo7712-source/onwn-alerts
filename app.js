997d9865bfa60cbd6c6628f7afd2fc259712c65f
adammcadoo7712-source <adam.mcadoo.7712@gmail.com>
2026-01-10 21:34:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-<body>
-  <h1>ONWN Weather Alerts</h1>
+function testAlert() {
+  alert("JavaScript is working!");
+}
 
-  <button onclick="enableAlerts()">Enable Weather Alerts</button>
-
-  <p id="status"></p>
-
-  <script src="app.js"></script>
-</body>
+console.log("app.js loaded");
