6d5588d2880647706bec400f26f9c4f61eb4c243
adammcadoo7712-source <adam.mcadoo.7712@gmail.com>
2026-01-11 11:34:04 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,8 +2,14 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  // WILL FILL THIS IN NEXT STEP
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyBbZvjahyUjZ1g6Vk3q95J9sehhyDvzALE",
+  authDomain: "onwn-weather-alerts.firebaseapp.com",
+  projectId: "onwn-weather-alerts",
+  storageBucket: "onwn-weather-alerts.firebasestorage.app",
+  messagingSenderId: "114204669848",
+  appId: "1:114204669848:web:950f8d25a25b65da419ded"
+};
 
 const messaging = firebase.messaging();
 
