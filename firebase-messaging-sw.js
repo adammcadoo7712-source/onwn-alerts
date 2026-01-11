8db79294424402ea53e5eb41f382ef7a05149d0b
adammcadoo7712-source <adam.mcadoo.7712@gmail.com>
2026-01-11 13:10:19 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,19 +2,18 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-const firebaseConfig = {
   apiKey: "AIzaSyBbZvjahyUjZ1g6Vk3q95J9sehhyDvzALE",
   authDomain: "onwn-weather-alerts.firebaseapp.com",
   projectId: "onwn-weather-alerts",
   storageBucket: "onwn-weather-alerts.firebasestorage.app",
   messagingSenderId: "114204669848",
   appId: "1:114204669848:web:950f8d25a25b65da419ded"
-};
+});
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("Background message received", payload);
+  console.log("Background message received:", payload);
 
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
@@ -27,4 +26,3 @@ messaging.onBackgroundMessage((payload) => {
     notificationOptions
   );
 });
-
