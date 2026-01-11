1022c1ee05a14d38be476ef6ee8a3d49f62a70d8
adammcadoo7712-source <adam.mcadoo.7712@gmail.com>
2026-01-11 10:56:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -28,3 +28,21 @@ document.addEventListener("DOMContentLoaded", () => {
     );
   });
 });
+function isPointInPolygon(point, polygon) {
+  let inside = false;
+  const [x, y] = point;
+
+  for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+    const xi = polygon[i][0], yi = polygon[i][1];
+    const xj = polygon[j][0], yj = polygon[j][1];
+
+    const intersect =
+      ((yi > y) !== (yj > y)) &&
+      (x < ((xj - xi) * (y - yi)) / (yj - yi) + xi);
+
+    if (intersect) inside = !inside;
+  }
+
+  return inside;
+}
+
